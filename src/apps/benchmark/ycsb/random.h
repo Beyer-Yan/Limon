cb31db249690236a882cad1f81ff91a5e6f6e226
yanbaoyue <yanbaoyue@sensetime.com>
2020-05-31 00:14:20 +0800

    compile error fixed

--- src/apps/benchmark/ycsb/random.h
+++ src/apps/benchmark/ycsb/random.h
@@ -6,13 +6,24 @@ typedef long (*random_gen_t)(void);
 unsigned long xorshf96(void);
 unsigned long locxorshf96(void);
 
-void init_seed(void); // must be called after each thread creation
+// must be called after each thread creation
+void init_seed(void); 
 void init_zipf_generator(long min, long max);
-long zipf_next(void); // zipf distribution, call init_zipf_generator first
-long uniform_next(void); // uniform, call init_zipf_generator first
-long bogus_rand(void); // returns something between 1 and 1000
-long production_random1(void); // production workload simulator
-long production_random2(void); // production workload simulator
+
+// zipf distribution, call init_zipf_generator first
+long zipf_next(void); 
+
+// uniform, call init_zipf_generator first
+long uniform_next(void); 
+
+ // returns something between 1 and 1000
+long bogus_rand(void);
+
+// production workload simulator
+long production_random1(void); 
+
+// production workload simulator
+long production_random2(void); 
 
 const char *get_function_name(random_gen_t f);
 #endif
