700741a9064967ea789688ec16997f15d54614a1
yanbaoyue <yanbaoyue@sensetime.com>
2020-06-04 02:31:00 +0800

    restore the chunkmgr implementation

--- src/pagechunk/chunkmgr.c
+++ src/pagechunk/chunkmgr.c
@@ -10,22 +10,9 @@
 
 #include "../worker/worker_internal.h"
 
-struct mem_node{
-    struct chunk_mem *mem;
-    LIST_ENTRY(mem_node) link;
-};
-
-struct chunk_mem_pool{
-    LIST_HEAD(,mem_node) free_head;
-    struct object_cache_pool *mem_node_pool;
-};
-
 //Sinleton mode
 static struct chunkmgr_worker_context g_chunkmgr_worker;
-
-//simple but efficient global chunk memory pool.
-static struct chunk_mem_pool _g_mem_pool;
-//static struct object_cache_pool  *_g_mem_pool;
+static struct object_cache_pool  *_g_mem_pool;
 
 //Allocate chunk memory in one-shot mode.
 static void
@@ -35,50 +22,11 @@ _chunk_mem_init(uint64_t nb_chunks){
     uint32_t bitmap_size = bitmap_header_size(g_chunkmgr_worker.nb_pages_per_chunk);
     uint32_t mem_hdr_size = KV_ALIGN(sizeof(struct chunk_mem)+bitmap_size,0x1000u);
     uint32_t chunk_mem_size = mem_hdr_size + chunk_data_size;
-    
-    SPDK_NOTICELOG("Reserved %lu total bytes in chunk cache\n", chunk_mem_size*nb_chunks);
 
-    //It always fails to allocate large continious dma memory, so I no choice other than
-    //splitting the memory allocating into every chunk memory.
-    uint64_t pool_hdr_size = pool_header_size(nb_chunks);
-    uint8_t* pool_data = malloc(pool_hdr_size + sizeof(struct mem_node)*nb_chunks);
-    assert(pool_data!=NULL);
+    uint64_t pool_hdr_size = KV_ALIGN(pool_header_size(nb_chunks),0x1000u);
+    uint64_t total_bytes = pool_hdr_size + chunk_mem_size*nb_chunks;
 
-    _g_mem_pool.mem_node_pool = pool_data;
-    pool_data += pool_hdr_size;
-    pool_header_init(_g_mem_pool.mem_node_pool,nb_chunks,
-                    sizeof(struct mem_node),pool_hdr_size,pool_data);
-
-    LIST_INIT(&_g_mem_pool.free_head);
-
-    uint8_t* test = spdk_memzone_reserve_aligned("chunk_cache",nb_chunks*chunk_mem_size,SPDK_ENV_SOCKET_ID_ANY,SPDK_MEMZONE_NO_IOVA_CONTIG,0x1000);
-    if(!test){
-        printf("mem reserving failds, bytes:%lu\n",nb_chunks*chunk_mem_size);
-    }
-    eixt(-1);
-
-    uint64_t i = 0;
-    for(;i<nb_chunks;i++){
-        struct chunk_mem* mem = spdk_malloc(chunk_mem_size,0x1000,NULL,
-                                            SPDK_ENV_SOCKET_ID_ANY,SPDK_MALLOC_DMA);
-        if(!mem){
-            SPDK_ERRLOG("Faild to allocate memory, number:%lu, bytes:%lu\n",i,chunk_mem_size);
-            exit(-1);
-        }
-
-        mem->nb_bytes = chunk_data_size;
-        mem->data = (uint8_t*)(mem) + mem_hdr_size;
-        mem->bitmap[0].length = g_chunkmgr_worker.nb_pages_per_chunk;
-
-        struct mem_node *node = pool_get(_g_mem_pool.mem_node_pool);
-        assert(node!=NULL);
-
-        node->mem = mem;
-        LIST_INSERT_HEAD(&_g_mem_pool.free_head,node,link);
-    }
-
-    /*
-    //uint8_t* data = spdk_malloc(total_bytes,0x1000, NULL, SPDK_ENV_LCORE_ID_ANY,
+    uint8_t* data = spdk_malloc(total_bytes,0x1000, NULL, SPDK_ENV_LCORE_ID_ANY,
 						SPDK_MALLOC_DMA);
     if(!data){
         SPDK_ERRLOG("Faild to allocate memory, chunks:%u, bytes:%lu\n",nb_chunks,total_bytes);
@@ -96,28 +44,21 @@ _chunk_mem_init(uint64_t nb_chunks){
         mem->data = (uint8_t*)(mem) + mem_hdr_size;
         mem->bitmap[0].length = g_chunkmgr_worker.nb_pages_per_chunk;
     }
-    */
 }
 
 static struct chunk_mem *
 _get_one_chunk_mem(void){
-    struct mem_node *node  = LIST_FIRST(&_g_mem_pool.free_head);
-    if(node){
-        //There is free chunk memory
-        pool_release(_g_mem_pool.mem_node_pool,node);
-        return node->mem;
+    struct chunk_mem* mem = pool_get(_g_mem_pool);
+    if(!mem){
+        return NULL;
     }
-    return NULL;
+    return mem;
 }
 
 static void
 _release_one_chunk_mem(struct chunk_mem *mem){
     assert(mem!=NULL);
-    struct mem_node *node = pool_get(_g_mem_pool.mem_node_pool);
-    assert(node!=NULL);
-
-    node->mem = mem;
-    LIST_INSERT_HEAD(&_g_mem_pool.free_head,node,link);
+    pool_release(_g_mem_pool,mem);
 }
 
 static struct worker_context* _chunkmgr_evaluate_worklaod(void){
@@ -257,9 +198,9 @@ _do_destroy(void*ctx){
     spdk_thread_destroy(g_chunkmgr_worker.thread);
     g_chunkmgr_worker.thread = NULL;
 
-    //Release all the chunk memory ??? Maybe unnecessary.
-    //free(_g_mem_pool.mem_node_pool);
-
+    //Release all the chunk memory
+    spdk_free(_g_mem_pool);
+    _g_mem_pool = NULL;
     SPDK_NOTICELOG("chunkmgr worker destroyed\n");
 }
 
