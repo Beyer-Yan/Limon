db8d76fde1bba5443007c6dcc9e15a52348aeeff
yanbaoyue <yanbaoyue@sensetime.com>
2020-06-09 13:44:56 +0800

    add lantecy histogram

--- src/apps/benchmark/ycsb/histogram.c
+++ src/apps/benchmark/ycsb/histogram.c
@@ -4,6 +4,7 @@
 
 static uint64_t g_tsc_rate;
 static struct spdk_histogram_data *g_his_data;
+static double g_min,g_max,g_avg;
 
 static const double g_latency_cutoffs[] = {
 	0.01,
@@ -35,9 +36,15 @@ _check_cutoff(void *ctx, uint64_t start, uint64_t end, uint64_t count,
 		return;
 	}
 
+    double cur = (double)end * 1000 * 1000 / g_tsc_rate;
+
+    g_min = cur < g_min ? cur : g_min;
+    g_max = cur > g_max ? cur : g_max;
+    g_avg += cur/total;
+
 	so_far_pct = (double)so_far / total;
 	while (so_far_pct >= **cutoff && **cutoff > 0) {
-		printf("%9.5f%% : %9.3fus\n", **cutoff * 100, (double)end * 1000 * 1000 / g_tsc_rate);
+		printf("%9.5f%% : %9.3fus\n", **cutoff * 100, cur);
 		(*cutoff)++;
 	}
 }
@@ -49,6 +56,9 @@ void histogram_init(void){
 
 void histogram_reset(void){
     assert(g_his_data!=NULL);
+    g_min = 0;
+    g_max = 0;
+    g_avg = 0;
     spdk_histogram_data_reset(g_his_data);
 }
 
@@ -60,6 +70,7 @@ void histogram_update(uint64_t data){
 void histogram_print(void){
     assert(g_his_data!=NULL);
     printf("Summary latency histogram\n");
+    printf("min:%9.3fus avg:%9.3fus max: %9.3fus\n",g_min,g_avg,g_max);
     printf("=================================================================================\n");
 
     const double *cutoff = g_latency_cutoffs;
