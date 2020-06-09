386d141b370b92ba4c5c5a807210946d9fc7760a
yanbaoyue <yanbaoyue@sensetime.com>
2020-06-09 13:58:52 +0800

    remove avg histogram

--- src/apps/benchmark/ycsb/histogram.c
+++ src/apps/benchmark/ycsb/histogram.c
@@ -4,7 +4,6 @@
 
 static uint64_t g_tsc_rate;
 static struct spdk_histogram_data *g_his_data;
-static double g_min,g_max,g_avg;
 
 static const double g_latency_cutoffs[] = {
 	0.01,
@@ -38,10 +37,6 @@ _check_cutoff(void *ctx, uint64_t start, uint64_t end, uint64_t count,
 
     double cur = (double)end * 1000 * 1000 / g_tsc_rate;
 
-    g_min = cur < g_min ? cur : g_min;
-    g_max = cur > g_max ? cur : g_max;
-    g_avg += cur/total;
-
 	so_far_pct = (double)so_far / total;
 	while (so_far_pct >= **cutoff && **cutoff > 0) {
 		printf("%9.5f%% : %9.3fus\n", **cutoff * 100, cur);
@@ -56,9 +51,6 @@ void histogram_init(void){
 
 void histogram_reset(void){
     assert(g_his_data!=NULL);
-    g_min = 0;
-    g_max = 0;
-    g_avg = 0;
     spdk_histogram_data_reset(g_his_data);
 }
 
@@ -74,9 +66,6 @@ void histogram_print(void){
 
     const double *cutoff = g_latency_cutoffs;
     spdk_histogram_data_iterate(g_his_data, _check_cutoff, &cutoff);
-    printf("min:%9.3fus\n",g_min);
-    printf("avg:%9.3fus\n",g_avg);
-    printf("max:%9.3fus\n",g_max);
     printf("\n");
 }
 
